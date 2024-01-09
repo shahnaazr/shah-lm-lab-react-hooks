dad38d78835867e1e62a2da759c8c50814f3266a
Shahnaaz Rahamatullah <rahamsha@gmail.com>
2024-01-09 14:55:01 +0000

    feat: add use ref to component

--- src/components/hooks/use_ref/count_renders.tsx
+++ src/components/hooks/use_ref/count_renders.tsx
@@ -1,21 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 export const CountRenders = () => {
-	const [value, setValue] = useState('');
-	const count = 1;
+  const [value, setValue] = useState("");
+  const count = useRef(1);
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
 
-	return (
-		<>
-			<h2>useRef</h2>
+  return (
+    <>
+      <h2>useRef</h2>
 
-			<input
-				value={value}
-				type='text'
-				onChange={(e) => setValue(e.target.value)}
-			/>
+      <input
+        value={value}
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+      />
 
-			<p>{value}</p>
-			<p>I have rendered {count} times</p>
-		</>
-	);
+      <p>{value}</p>
+      <p>I have rendered {count.current} times</p>
+    </>
+  );
 };
