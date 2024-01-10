4b6484f1cf018e7d330932f95cadb5cd719f9a19
Shahnaaz Rahamatullah <rahamsha@gmail.com>
2024-01-10 16:43:14 +0000

    feat: add useEffect to components

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,34 @@
 // make an api call using useEffect
 
-export const APICall = () => {
+import { useEffect, useState } from "react";
 
-    // Your code here!
+interface Todo {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+}
+
+export const APICall = () => {
+const [json, setJson]= useState<Todo|null>(null);
+useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+            const json = await response.json();
+            // console.log(json);
+            setJson(json)
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    fetchData()
+}, [])
 
     return (
         <>
             <h2>useEffect</h2>
-            <p>Your todo here!</p>
+            <p>{json?.title}</p>
         </>
     )
 }
